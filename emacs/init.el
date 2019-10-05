edfa1292112909e098d24187ea0cf3ea7715a87d
Orcan Kolankaya <orcankolankaya@gmail.com>
2019-10-05 16:35:49 +0300

    added fuzzy finder path and default cmd from zsh to emacs config file myconfig. Also init.el is changed

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 ;; Bootstrap 'use-package'
 (package-initialize)
@@ -11,3 +12,21 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myconfig.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update beacon 0blayout cheatsheet evil which-key use-package undo-tree try tangotango-theme powerline pdf-tools org-bullets counsel auto-complete auctex ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "deep sky blue")))))
+(put 'narrow-to-region 'disabled nil)

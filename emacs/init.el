4b6015a6e58d031a18caba6271d62704280bf41c
Orcan Kolankaya <orcankolankaya@gmail.com>
2019-10-13 19:31:42 +0300

    emacs new init.el and theming in myconfig

--- emacs/init.el
+++ emacs/init.el
@@ -7,9 +7,23 @@
 ;; Bootstrap 'use-package'
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+(mapc
+ (lambda (package)
+   (if (not (package-installed-p package))
+       (progn
+         (package-refresh-contents)
+         (package-install package))))
+ '(use-package diminish bind-key))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+(setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myconfig.org"))
 (custom-set-variables
@@ -22,7 +36,7 @@
     ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update beacon 0blayout cheatsheet evil which-key use-package undo-tree try tangotango-theme powerline pdf-tools org-bullets counsel auto-complete auctex ace-window))))
+    (dired-subtree gnu-elpa-keyring-update beacon 0blayout cheatsheet evil which-key use-package undo-tree try tangotango-theme powerline pdf-tools org-bullets counsel auto-complete auctex ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

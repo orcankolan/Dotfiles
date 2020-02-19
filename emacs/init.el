0872f61fbcdf6878c24681ee658efe64a9328413
Orcan Kolankaya <orcankolankaya@gmail.com>
2020-02-19 23:47:17 +0300

    emacs init.el modified

--- emacs/init.el
+++ emacs/init.el
@@ -26,21 +26,3 @@
 (setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myconfig.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(package-selected-packages
-   (quote
-    (dired-subtree gnu-elpa-keyring-update beacon 0blayout cheatsheet evil which-key use-package undo-tree try tangotango-theme powerline pdf-tools org-bullets counsel auto-complete auctex ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0 :foreground "deep sky blue")))))
-(put 'narrow-to-region 'disabled nil)

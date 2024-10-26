20eb7667cae2c84468a6a09a0b9cc22bd62252e6
Orcan Kolankaya <orcankolankaya@gmail.com>
2024-10-26 10:03:03 +0300

    permpsm edited

--- permpsm.sh
+++ permpsm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-chown -R admin:www-data ./psm-portal
-# ndt-portal folder first
+chown -R admin:admin ./psm-portal
+# psm-portal folder first
 find ./psm-portal -type d -exec chmod u=rwx,g=rx,o= '{}' +
 find ./psm-portal -type f -exec chmod u=rw,g=r,o= '{}' +
-# ndt-portal/storage second
+# psm-portal/storage second
 find ./psm-portal/storage -type d -exec chmod u=rwx,g=rwx,o= '{}' +
 find ./psm-portal/storage -type f -exec chmod u=rw,g=rw,o= '{}' +
-# ndt-portal/bootstarp/cache third
+# psm-portal/bootstarp/cache third
 find ./psm-portal/bootstrap/cache -type d -exec chmod u=rwx,g=rwx,o= '{}' +
 find ./psm-portal/bootstrap/cache -type f -exec chmod u=rw,g=rw,o= '{}' +
 
